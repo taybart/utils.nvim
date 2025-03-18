1b472d118109686f3283efd9cb59d67b0b1f53fd
Taylor Bartlett <taybart@gmail.com>
2025-03-17 21:11:48 -0600

    feat: add extract_path_end

--- lua/taybart/utils/init.lua
+++ lua/taybart/utils/init.lua
@@ -12,4 +12,18 @@ function M.file_exists(name)
   end
 end
 
+function M.extract_path_end(path, depth)
+  local components = {}
+  for part in path:gmatch('[^/]+') do
+    table.insert(components, part)
+  end
+
+  local result = {}
+  for i = math.max(1, #components - depth + 1), #components do
+    table.insert(result, components[i])
+  end
+
+  return table.concat(result, '/')
+end
+
 return M
